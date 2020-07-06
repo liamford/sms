03fd717e39e74b14ec84a66faa9de35d786a8990
Liam Ford <liam.ford.au@gmail.com>
2020-07-06 16:37:19 +1000

    removed unnecessary value

--- src/main/java/au/com/medaforum/twilioSms/listener/NotificationReceiver.java
+++ src/main/java/au/com/medaforum/twilioSms/listener/NotificationReceiver.java
@@ -1,7 +1,6 @@
 package au.com.medaforum.twilioSms.listener;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +10,6 @@ import javax.jms.JMSException;
 @Component
 @Slf4j
 public class NotificationReceiver {
-
-    @Value("${amazon.sqs}")
-    private String sqs;
 
     @JmsListener(destination = "${amazon.sqs}")
     public void receive(String json) throws JMSException {
