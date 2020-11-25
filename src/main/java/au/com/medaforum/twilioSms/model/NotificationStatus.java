138b633b1617d67465b1f7b6f9b53d4518b19c26
Liam Ford <liam.ford.au@gmail.com>
2020-11-26 00:19:10 +1100

    corrected Notification status

--- src/main/java/au/com/medaforum/twilioSms/model/NotificationStatus.java
+++ src/main/java/au/com/medaforum/twilioSms/model/NotificationStatus.java
@@ -14,7 +14,7 @@ public enum NotificationStatus {
     QUESTION(0, "Question"),
     MORE_INFO_REQUEST(1, "More Information"),
     MORE_INFO_ANSWERED(2, "More Information"),
-    ANSWER(2, "Answer");
+    ANSWER(3, "Answer");
     private static final Map<Integer, String> NOTIFICATION_TYPE_MAP = new HashMap<Integer, String>(3);
 
     static {
